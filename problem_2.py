bcbf440c6f586b2bac9f301a053b884292a40c65
YoonDongHyeon <dhdh9595@naver.com>
2016-10-25 22:11:11 +0900

    wndrks

--- problem_2.py
+++ problem_2.py
@@ -26,7 +26,7 @@ while True:
         for k in range(0,7):
             blank[i].append(machine_part[k][i]*(2**(k+1)))
         sum_verticalline.append(sum(blank[i]))
-
+    # 2의 몇승을 곱해주는 작업과 그것들의 합을 구하는 과정
     for i in range(0,8):
         for j in range(0,7):
             if i == 0:
@@ -35,8 +35,7 @@ while True:
             else:
                 totalmatrix[i].append(machine_part[i-1][j])
                 totalmatrixsub[i].append(sum_verticalline[j])
-    # print(totalmatrix)
-    # print(sum_verticalline)
+    # 확장 행렬을 써서 합을 행렬에 추가하는 작업
 
     for k in range(0,8):
         for i in range(0,6):
@@ -46,25 +45,27 @@ while True:
                         t=totalmatrix[0][i]
                         totalmatrix[0][i]=totalmatrix[0][j]
                         totalmatrix[0][j]=t
+                #         sorting 해주는 과정
                 else:
                     break
-    for l in range(0,7):
-        if totalmatrix[0][l] != totalmatrixsub[0][l]:
-            for k in range(1, 8):
-                for i in range(0, 6):
-                    for j in range(1, 7):
-                        if totalmatrix[0][i] == totalmatrixsub[0][j]:
-                            t = totalmatrix[k][i]
-                            totalmatrix[k][i] = totalmatrix[k][j]
-                            totalmatrix[k][j] = t
-            machine_part = [[], [], [], [], [], [], []]
-            for i in range(0,7):
-                for k in range(1,8):
-                    machine_part[i].append(totalmatrix[k][i])
-            print(machine_part)
-            continue
-        else:
-            break
+    # for l in range(0,7):
+    #     if totalmatrix[0][l] != totalmatrixsub[0][l]:
+        for k in range(1, 8):
+            for i in range(0, 6):
+                for j in range(1, 7):
+                    if totalmatrix[0][i] == totalmatrixsub[0][j]:
+                        t = totalmatrix[k][i]
+                        totalmatrix[k][i] = totalmatrix[k][j]
+                        totalmatrix[k][j] = t
+        #                 
+        machine_part = [[], [], [], [], [], [], []]
+        for i in range(0,7):
+            for k in range(1,8):
+                machine_part[i].append(totalmatrix[k][i])
+        print(machine_part)
+            # continue
+        # else:
+        #     break
 
 
 
